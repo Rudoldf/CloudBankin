7c055aa1490283c9335e6a6359e1ccdd424435bd
RUDOLDF GONZALEZ <rudoldf@hotmail.com>
2021-04-22 02:20:59 -0300

    update

--- CloudBanking.Core/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace CloudBanking.Core
-{
-    public class Class1
-    {
-    }
-}
--- CloudBanking.Infrastructure/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace CloudBanking.Infrastructure
-{
-    public class Class1
-    {
-    }
-}
